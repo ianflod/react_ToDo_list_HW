d6b208c77e1f7f199b3e8a2a26514bbef1fc3495
CodeClan Student <codeclanstudent@codeclans-mbp.home>
2022-06-14 10:56:21 +0100

    All functioning

--- src/App.js
+++ src/App.js
@@ -4,39 +4,39 @@ import './App.css';
 function App() {
 
   const [tasks, setTasks] = useState([
-    { name: "Wash Dishes", priority: "High" },
-    { name: "Walk Cat", priority: "Low" },
-    { name: "Cut Grass", priority: "High" }
+    { name: "Wash Dishes", priority: "high" },  //BEWARE of caps and spelling for names/priorities etc
+    { name: "Walk Cat", priority: "low" },
+    { name: "Cut Grass", priority: "high" }
   ]
   );
 
   const [newTask, setNewTask] = useState("");
   const [newPri, setNewPri] = useState("");
 
-  const taskNodes = tasks.map((task, index) => {
+  const taskNodes = tasks.map((task, index) => {   ///loops through our tasks and creates new array
     return (
-
-      <li key={index} className="low"><span>{task.name}</span><span>{task.priority}</span></li>
-
+      <li key={index} className={task.priority}>{task.name}</li>
     )
   });
 
 
 
-  const handleTaskInput = (event) => {
+  const handleTaskInput = (event) => {  ///HANDLES the input fromn the form
     setNewTask(event.target.value)
   }
 
-  const handlePriorityInput = (event) => {
+  const handlePriorityInput = (event) => {  ///Handles input from form for priority
     setNewPri(event.target.value)
   }
 
   const saveNewtask = (event) => {
-    event.preventDefault();
-    const copyTasks = [...tasks]
-    copyTasks.push({ name: newTask, priority: newPri })
-    setTasks(copyTasks)
-    setNewTask("")
+    event.preventDefault();      // STOPS the automatic POST function
+    const copyTasks = [...tasks]  //Creates copy of original array, we dont want to manipulate the orignal
+    copyTasks.push({ name: newTask, priority: newPri });  // pushes the new task into the array
+    setTasks(copyTasks);  /// THIS SETS the new array to be THE array 
+    setNewTask("");
+    setNewPri("");
+    event.target.reset(); ///RESETS the form to blank
   }
 
 
@@ -50,13 +50,13 @@ function App() {
       <form onSubmit={saveNewtask}>
         <label htmlFor="new-task"></label>
         <input id="new-task" type="text" value={newTask} onChange={handleTaskInput} />
-        <input type="submit" value="Save New Task" />
+        <input type="submit" value="Save New Task" />  {/*THIS is the enter button*/}
 
         <label htmlFor="high">High</label>
-        <input type="radio" value="High" name="priority" onChange={handlePriorityInput} />
+        <input type="radio" id="high" value="high" name="priority" onChange={handlePriorityInput} /> {/* ENSURE name is the sme on both radio buttons*/}
 
         <label htmlFor="low">Low</label>
-        <input type="radio" value="Low" name="priority" onChange={handlePriorityInput} />
+        <input type="radio" id="low" value="low" name="priority" onChange={handlePriorityInput} /> {/*ONCHANGE is handler for new state*/}
 
       </form>
 
